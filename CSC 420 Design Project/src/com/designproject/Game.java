1b4ba787b685d8caefbf96d6cb9a15ec4f7bf6e4
Dalton Vega <daltonv297@gmail.com>
2016-02-11 21:14:45 -0500

    tried to add graphics2d

---
+++ CSC 420 Design Project/src/com/designproject/Game.java
@@ -0,0 +1,5 @@
+package com.designproject;
+
+public class Game {
+
+}
--- CSC 420 Design Project/src/com/designproject/Graphics.java
+++
@@ -1,7 +0,0 @@
-package com.designproject;
-
-public class Graphics {
-	public static void main(String[] args) {
-		System.out.println("Test");
-	}
-}
