7c34ea974d689093b4359cc7ab2f5a28e3b75963
Dalton Vega <daltonv297@gmail.com>
2016-02-12 14:14:29 -0500

    progress

--- CSC 420 Design Project/src/com/designproject/Game.java
+++ CSC 420 Design Project/src/com/designproject/Game.java
@@ -1,5 +1,58 @@
 package com.designproject;
 
-public class Game {
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+	
+	public static final double aspectRatio = 16 / 9;
+	public static final int WIDTH = 1280;
+	public static final int HEIGHT = (int) (WIDTH * aspectRatio);
+	
+	public static final String NAME = "TreeFiddy";
+	
+	private JFrame frame;
+	
+	public boolean running = false;
+	
+	public Game() {
+		setMinimumSize(new Dimension(WIDTH, HEIGHT));
+		setMaximumSize(new Dimension(WIDTH, HEIGHT));
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		
+		frame = new JFrame(NAME);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		
+		frame.add(this, BorderLayout.CENTER);
+		frame.pack();
+		
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+	
+	public synchronized void start() {
+		new Thread(this).start();
+		running = true;
+	}
+	
+	public synchronized void stop() {
+		
+	}
+
+	public void run() {
+		
+	}
+	
+	public static void main(String[] args) {
+		new Game().start();
+	}
+	
 }
--- CSC 420 Design Project/src/com/designproject/WindowGraphics.java
+++ CSC 420 Design Project/src/com/designproject/WindowGraphics.java
@@ -1,48 +1,5 @@
 package com.designproject;
 
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.geom.Line2D;
- 
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
- 
-
-public class WindowGraphics extends JFrame {
+public class WindowGraphics {
 	
-	private int WIDTH = 1280;
-	private int HEIGHT = 720;
- 
-    public WindowGraphics() {
-        super("Lines Drawing Demo");
- 
-        setSize(WIDTH, HEIGHT);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-    }
- 
-    void drawToScreen(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
- 
-        g2d.drawLine(120, 50, 460, 90);
- 
-        g2d.draw(new Line2D.Double(59.2d, 99.8d, 419.1d, 99.8d));
- 
-        g2d.draw(new Line2D.Float(21.50f, 132.50f, 459.50f, 132.50f));
- 
-    }
- 
-    public void paint(Graphics g) {
-        super.paint(g);
-        drawToScreen(g);
-    }
- 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                new WindowGraphics().setVisible(true);
-            }
-        });
-    }
 }
